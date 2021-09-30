346dfbd019a175b7a286acc77c0600afa5cc4239
Waleed Javed <waleedjavied@gmail.com>
2021-09-30 13:59:56 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,9 +149,15 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+     resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingIds: [
+          "G-3V83N51X7F",
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
       },
     },
   ],
